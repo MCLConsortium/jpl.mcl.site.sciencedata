08c22850224560deb9f051fab3528d1475d20413
yuliujpl <david.liu@jpl.nasa.gov>
2017-02-07 07:38:15 -0800

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os.path
 # ------------
 
 _name            = 'jpl.mcl.site.sciencedata'
-_version         = '0.0.2'
+_version         = '0.0.3'
 _description     = 'Science data for the MCL site'
 _url             = 'https://github.com/MCLConsortium/' + _name
 _downloadURL     = 'https://github.com/MCLConsortium/' + _name + '/archive/' + _version + '.zip'
